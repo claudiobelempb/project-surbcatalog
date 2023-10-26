c9b57f25998f4c814a57eebbd5fdba1430394ec5
claudiobelempb <claudio.c.lima@hotmail.com>
2023-10-26 19:45:21 -0300

    first commit

---
+++ backend/src/main/java/br/com/surb/surbcatalog/SurbcatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.surbcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurbcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurbcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/surb/surbcatalog/SurbcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surb.surbcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurbcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
